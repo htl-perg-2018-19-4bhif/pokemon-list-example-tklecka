4fd052ae2c5fab286c4eb6d0eef4a76ff1386368
tklecka <tim@klecka.at>
2018-12-08 10:59:10 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
+import { PokeDetailsComponent } from './poke-details/poke-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokeListComponent,
+    PokeDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/poke-details/poke-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-details',
+  templateUrl: './poke-details.component.html',
+  styleUrls: ['./poke-details.component.css']
+})
+export class PokeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
