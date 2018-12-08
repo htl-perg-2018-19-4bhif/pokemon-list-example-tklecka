f13525a4fe8b6c75ebc644f94e1e1c17bbf3740b
tklecka <tim@klecka.at>
2018-12-08 11:07:32 +0100

    Api Service added

--- src/app/poke-api.service.ts
+++ src/app/poke-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
 
+const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8', 'Accept': 'application/json' }) };
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private baseURL = 'https://pokeapi.co/api/v2/';
+
+  get(url: string): Observable<Object> {
+    const endpointUrl = `${this.baseURL}${url}`;
+    return this.http
+      .get(endpointUrl);
+  }
+
 }
